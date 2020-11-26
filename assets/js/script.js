4049af5b9d79b2e4135131a4d5a11bf4f394961e
Jonathan Ferreira <jhouus33@gmail.com>
2020-11-25 22:04:27 -0300

    Script ok

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+/*
+getElementByTagName()
+getElementByTagName()
+getElementsByClassName()
+querySelector()
+
+*/
+
+/* index  */
+
+
+
+/* contato */
+
+let nome = document.getElementById('nome')
+let email = document.getElementById('email')
+let assunto = document.getElementById('assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+
+nome.style.height = '20px';
+email.style.height = '20px';
+
+function validaNome() {
+    let txtName = document.querySelector("#txtNome")
+    if(nome.value.length < 3){
+        txtName.innerHTML = 'Nome completo pf!'
+        txtName.style.color = 'red'
+    } else{
+        nome.style.cssText = 'border-color: green;'
+        txtNome.innerHTML = ""
+        nomeOk = true
+    }
+
+}
+    function validaEmail() {
+        let txtEmail = document.querySelector("#txtEmail")
+        if(email.value.indexOf('.') == -1){
+            txtEmail.innerHTML = 'E-mail inválido, deve conter: @'
+            txtEmail.style.color = 'red'
+        } else{
+            email.style.cssText = 'border-color: green;'
+            txtEmail.innerHTML = ""
+            emailOk = true
+        }
+
+    }
+
+    function validaAssunto() {
+        let txtAssunto = document.querySelector('#txtAssunto')
+        if (assunto.value.length > 100) {
+            assunto.style.cssText = 'border-color: red;'
+        } else {
+            assunto.style.cssText = 'border-color: green;'
+            txtAssunto.style.display = 'none'
+            assuntoOk = true
+        }
+    }
+
+
+    function enviar() {
+        if (nomeOk == true && emailOk == true && assuntoOk ==true){
+            alert("Formulário enviado")
+        } else {
+            alert('Verificar campos formulário')
+        }
+    }
